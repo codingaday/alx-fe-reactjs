7ee04293299102aed8dadd899d4c3c818892ffe0
Abebe Duguma <abebeduguma27@gmail.com>
2025-02-17 21:08:26 +0300

    UserProfile component added

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,10 +2,12 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile ";
 
 function App() {
   return (
     <>
+      <UserProfile name="Abebe" age="28" bio="Loves hiking and coding" />
       <WelcomeMessage />
       <Header />
       <MainContent />
