392eddc3882867b7fdad71aaa2e71d5b416408b8
Abebe Duguma <abebeduguma27@gmail.com>
2025-02-18 09:41:46 +0300

    bug fix

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,17 @@
+import UserProfile from "./components/UserProfile ";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile ";
 
 function App() {
   return (
     <>
+      <UserProfile name="Abebe" age="28" bio="Loves hiking and coding" />
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   );
 }
--- alx-react-app/src/components/UserProfile .jsx
+++ alx-react-app/src/components/UserProfile .jsx
@@ -1,11 +1,11 @@
-function UserProfile({ name, age, bio }) {
+const UserProfile = (props) => {
   return (
     <div>
-      <h2>{name}</h2>
-      <p>age: {age}</p>
-      <p>bio: {bio}</p>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
     </div>
   );
-}
+};
 
 export default UserProfile;
