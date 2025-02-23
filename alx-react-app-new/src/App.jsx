f703af56bce20cbfd9b1a802883665a7aa958057
Abebe Duguma <abebeduguma27@gmail.com>
2025-02-23 23:16:32 +0300

    new file Counter added

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,12 +4,14 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
 function App() {
   return (
     <>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <WelcomeMessage />
+      <Counter />
       <Header />
       <MainContent />
       <Footer />
